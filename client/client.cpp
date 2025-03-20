670a9455d4328387acb1a05abf4b145e34e960a8
sujahat59 <mali1237@conestogac.on.ca>
2025-03-20 15:11:37 -0400

    Implemented client utilities and refactored client.cpp

--- client/client.cpp
+++ client/client.cpp
@@ -1,53 +1,30 @@
 #include <iostream>
-#include <cstring>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include "client_utils.h"
 
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT 8080
 
 int main() {
-    int sock = 0;
-    struct sockaddr_in server_address;
-    char buffer[1024] = {0};
+    int sock = ClientUtils::createSocket();
+    if (sock < 0) return -1;
 
-    // Create socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        std::cerr << "Error: Socket creation failed!\n";
-        return -1;
-    }
-
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(SERVER_PORT);
-    inet_pton(AF_INET, SERVER_IP, &server_address.sin_addr);
-
-    // Connect to the server
-    if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        std::cerr << "Error: Connection to server failed!\n";
-        return -1;
-    }
+    if (!ClientUtils::connectToServer(sock, SERVER_IP, SERVER_PORT)) return -1;
 
     std::cout << "Connected to SecureLink Server!\n";
 
-    // Get username and password input
     std::string username, password;
     std::cout << "Enter Username: ";
     std::cin >> username;
     std::cout << "Enter Password: ";
     std::cin >> password;
 
-    // Send credentials to the server
     std::string credentials = username + ":" + password;
-    send(sock, credentials.c_str(), credentials.length(), 0);
+    ClientUtils::sendMessage(sock, credentials);
 
-    // Wait for authentication response
-    read(sock, buffer, 1024);
-    std::cout << "Server Response: " << buffer << "\n";
+    std::string response = ClientUtils::receiveMessage(sock);
+    std::cout << "Server Response: " << response << std::endl;
 
-    // Close socket
-    close(sock);
+    ClientUtils::closeSocket(sock);
     std::cout << "Connection closed.\n";
 
     return 0;
