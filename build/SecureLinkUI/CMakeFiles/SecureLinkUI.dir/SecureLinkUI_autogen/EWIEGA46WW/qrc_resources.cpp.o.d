08d7e7c5cc118a9331cea0e3759b2fc6e90e0f46
sujahat59 <mali1237@conestogac.on.ca>
2025-04-10 18:38:36 -0400

    🧹 Cleanup: Removed build files, junk, and unnecessary leftovers before submission

--- build/SecureLinkUI/CMakeFiles/SecureLinkUI.dir/SecureLinkUI_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-SecureLinkUI/CMakeFiles/SecureLinkUI.dir/SecureLinkUI_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
-  /Users/ruqi/secured/Project4_Group8/build/SecureLinkUI/SecureLinkUI_autogen/EWIEGA46WW/qrc_resources.cpp
